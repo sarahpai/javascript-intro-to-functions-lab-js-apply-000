3a31e300d1b13baeb31430ce2d43a9c567560b4c
Sarah Pai <pai.sarah89@gmail.com>
2018-08-28 13:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  var greeting = "I love you, grandmad"
   if (string.toLowerCase()){
   return "I can't hear you!"
   } else if (string.toUpperCase()){
